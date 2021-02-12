d70615fdb996ee6b0144afeb7a619b3d65aadf18
SihleKambula <kambulasihle@gmail.com>
2021-02-12 13:17:25 +0200

    url_shortener new site added to my website

--- data/projects.js
+++ data/projects.js
@@ -1,5 +1,23 @@
 const projects = [
   {
+    imageUrl: "/images/lilLink1.webp",
+    url: "https://lil-link.vercel.app/",
+    gitHubUrl: "https://github.com/SihleKambula/url_shorten_all",
+    name: "Lil Link",
+    description:
+      "A URL Shortener app. Users can also create an account and save the shorten link for future use.",
+    technologies: ["Next.js", "Node.js", "Firebase", "MongoDB"],
+  },
+  {
+    imageUrl: "/images/iqmates.webp",
+    url: "http://www.iqmates.com/",
+    gitHubUrl: null,
+    name: "IQmates",
+    description:
+      "IQmates is a eLearning platform for students who are interested in Maths, Computer Science, Art, Business etc. I was reponsible for the profile page, changed the UI and added in features such as checkbox completion for completed lessons. ",
+    technologies: ["JavaScript", "PHP", "MySQL", "Bootstrap"],
+  },
+  {
     imageUrl: "/images/racmMac.webp",
     url: "https://racm.co.za/",
     gitHubUrl: "https://github.com/SihleKambula/racm",
@@ -7,24 +25,6 @@ const projects = [
     description:
       "Rise African Child Media (RACM) is a creative digital platform that aims to tell stories in the TV and film industry, stories that can change the enviroment.",
     technologies: ["Next.js", "YouTube API", "Gsap.js"],
-  },
-  {
-    imageUrl: "/images/lilLinkMac.webp",
-    url: "https://lilink.netlify.app/",
-    gitHubUrl: "https://github.com/SKammy/Frontend-url-shorten.git",
-    name: "Lil Link",
-    description:
-      "A URL Shortener app. Users can also create an account and save the shorten link for future use.",
-    technologies: ["React.js", "Node.js", "Firebase", "MongoDB"],
-  },
-  {
-    imageUrl: "/images/iqmates.webp",
-    url: "http://www.iqmates.com/",
-    gitHubUrl: "/",
-    name: "IQmates",
-    description:
-      "Helped to develop IQmates with a great mentor. IQmates is a eLearning platform for students who are interested in Maths, Computer Science, Art, Business and etc. I was reponsible for the profile page, changed the UI and added in features like checkbox completion for completed lessons. ",
-    technologies: ["JavaScript", "PHP", "MySQL", "Bootstrap"],
   },
 ];
 export default projects;
