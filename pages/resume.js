ce1710c51f550d75b2e120cc207e5d1bca167ff1
SihleKambula <kambulasihle@gmail.com>
2021-04-19 19:46:25 +0200

    changed pdf cv and deleted unused pictures

--- pages/resume.js
+++ pages/resume.js
@@ -8,7 +8,7 @@ export default function Resume() {
       </Head>
       <div style={{ height: "100vh", overflow: "hidden" }}>
         <embed
-          src="/sihle.pdf"
+          src="/Sihle Kambula.pdf"
           type="application/pdf"
           style={{ width: "100%", height: "100%" }}
         />
