b87732f3be58593f01c870029c1163406a4dd61e
Sihle Kambula <kambulasihle@gamil.com>
2022-03-23 17:11:46 +0200

    project structure adjust

--- data/projects.js
+++ data/projects.js
@@ -1,20 +1,11 @@
 const projects = [
   {
-    imageUrl: "/images/lillink_bg.png",
-    url: "https://lil-link.vercel.app/",
-    gitHubUrl: "https://github.com/SihleKambula/url_shorten_all",
-    name: "Lil Link",
-    description:
-      "A URL Shortener app. Users can also create an account and save the shorten link for future use.",
-    technologies: ["React.JS", "Node.JS", "Firebase", "MongoDB"],
-  },
-  {
     imageUrl: "/images/IQmatesbg.png",
     url: "http://www.iqmates.com/",
     gitHubUrl: null,
     name: "IQmates",
     description:
-      "IQmates is a eLearning platform for students who are interested in Maths, Computer Science, Art, Business etc. I was reponsible for the profile page, changed the UI and added in features such as checkbox completion for completed lessons. ",
+      "IQmates is a eLearning platform and I was reponsible for the profile page redesign.",
     technologies: ["JavaScript", "PHP", "MySQL", "Bootstrap"],
   },
   {
@@ -25,6 +16,15 @@ const projects = [
     description:
       "ImageSafe is an application that allows you to save images to the cloud and access them from any device. ",
     technologies: ["React.JS", "Redux", "Firebase"],
+  },
+  {
+    imageUrl: "/images/lillink_bg.png",
+    url: "https://lil-link.vercel.app/",
+    gitHubUrl: "https://github.com/SihleKambula/url_shorten_all",
+    name: "Lil Link",
+    description:
+      "A URL Shortener app. Users can also create an account and save the shorten link for future use.",
+    technologies: ["React.JS", "Node.JS", "Firebase", "MongoDB"],
   },
 ];
 export default projects;
